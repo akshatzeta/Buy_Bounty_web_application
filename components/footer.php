cea49ed1de57c64baaa4a6457b1df9bdcf6b9417
akshatzeta <75921626+akshatzeta@users.noreply.github.com>
2023-11-06 10:16:31 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+
+   <section class="grid">
+      
+      <div class="box">
+         <h3>Zeta</h3>
+         <a>Akshat Choudhary NITW</a>
+         <a>Parag Mandal DSCE</a>
+      </div>
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i>HOME</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i>ABOUT</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i>SHOP</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i>LOGIN</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i>REGISTER</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i>CART</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i>ORDERS</a>
+      </div>
+
+   </section>
+
+</footer>
